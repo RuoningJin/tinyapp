05973ef02337a4afd8975354184262a97c688cc7
RuoningJin <ronjin2013@hotmail.com>
2022-10-31 22:22:19 -0400

    generaterandomstring function added

--- express_server.js
+++ express_server.js
@@ -8,7 +8,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
+const generateRandomString = () => {
+  const possChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  while (randomString.length < 6) {
+    randomString += possChar.charAt(Math.floor(Math.random() * possChar.length));
+  }
+  return randomString;
+};
 
 app.use(express.urlencoded({ extended: true }));
 
--- generateRandomString.js
+++ generateRandomString.js
@@ -1,4 +1,8 @@
 const generateRandomString = () => {
   const possChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
+  let randomString = '';
+  while (randomString.length < 6) {
+    randomString += possChar.charAt(Math.floor(Math.random() * possChar.length));
+  }
+  return randomString;
 };
